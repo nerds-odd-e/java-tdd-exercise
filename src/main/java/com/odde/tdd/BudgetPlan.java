f34f6748aa536bd2731348f88b97a5968a1dd2a9
zbcjackson <zbcjackson@gmail.com>
2020-07-25 16:20:16 +0800

    Cleanup messy log and type cast

--- src/main/java/com/odde/tdd/BudgetPlan.java
+++ src/main/java/com/odde/tdd/BudgetPlan.java
@@ -17,54 +17,33 @@ public class BudgetPlan {
     }
 
     public long query(LocalDate startDate, LocalDate endDate){
-        Logger.getLogger(this.getClass().getName()).info("Starting to calculate the budgeted amount for Budget between " + startDate + " and " + endDate + ".");
-
         //If Start and End are in the same budget period
         if (startDate.withDayOfMonth(1).equals(endDate.withDayOfMonth(1))){
-//			Logger.getLogger().info("Start Date and End Date are in the same period.");
             long amount = getBudgetAmount(startDate);
-//			Logger.getLogger().info("Amount = " + amount);
-            long daysInPeriod = getBudgetDaysCount(startDate);
-//			Logger.getLogger().info("Days in Period = " + daysInPeriod);
+           long daysInPeriod = getBudgetDaysCount(startDate);
             long daysBetween = startDate.until(endDate, DAYS) + 1;
-//			Logger.getLogger().info("Days Between = " + daysBetween);
-
-//			Logger.getLogger().info("Returning " + (long) (((double) amount / (double) daysInPeriod) * daysBetween));
-//			Logger.getLogger().info("Finished calculating the budget amount.\n\n");
             return amount / daysInPeriod * daysBetween;
         }
 
         //If the area between Start and End overlap at least two budget periods.
         if (YearMonth.from(startDate).isBefore(YearMonth.from(endDate))){
-//			Logger.getLogger().info("Start Date and End Date are in different budget periods.");
             long amountStartPeriod = getBudgetAmount(startDate);
-//			Logger.getLogger().info("Amount Start Period = " + amountStartPeriod);
             long daysInStartPeriod = getBudgetDaysCount(startDate);
-//			Logger.getLogger().info("Days in Start Period = " + daysInStartPeriod);
             long daysAfterStartDateInStartPeriod = startDate.until(startDate.withDayOfMonth(startDate.lengthOfMonth()), DAYS) + 1;
-//			Logger.getLogger().info("Days After Start Date in Start Period = " + daysAfterStartDateInStartPeriod);
-            double totalStartPeriod = (((double) amountStartPeriod / (double) daysInStartPeriod) * daysAfterStartDateInStartPeriod);
-//			Logger.getLogger().info("Total in Start Period = " + totalStartPeriod);
+            long totalStartPeriod =  amountStartPeriod / daysInStartPeriod * daysAfterStartDateInStartPeriod;
 
-            double totalInMiddle = 0;
+            long totalInMiddle = 0;
             for (Budget budget : getBudgetsBetween(startDate, endDate)) {
                 totalInMiddle += budget.getAmount();
                 Logger.getLogger(this.getClass().getName()).info("Added " + budget.getMonth() + " to total for one period in between; current value is " + totalInMiddle);
             }
-//			Logger.getLogger().info("Total in Middle = " + totalInMiddle);
 
             long amountEndPeriod = getBudgetAmount(endDate);
-//			Logger.getLogger().info("Amount End Period = " + amountEndPeriod);
             long daysInEndPeriod = getBudgetDaysCount(endDate);
-//			Logger.getLogger().info("Days in End Period = " + daysInEndPeriod);
             long daysBeforeEndDateInEndPeriod = endDate.getDayOfMonth();
-//			Logger.getLogger().info("Days before End Period = " + daysBeforeEndDateInEndPeriod);
-            double totalEndPeriod = (long) (((double) amountEndPeriod / (double) daysInEndPeriod) * daysBeforeEndDateInEndPeriod);
-//			Logger.getLogger().info("Total in End Period = " + totalEndPeriod);
+            long totalEndPeriod = amountEndPeriod /  daysInEndPeriod * daysBeforeEndDateInEndPeriod;
 
-//			Logger.getLogger().info("Sum of Start Period, Middle, and End Period = " + (totalStartPeriod + totalInMiddle + totalEndPeriod));
-//			Logger.getLogger().info("Finished Calculating the Budget Amount\n\n");
-            return (long) (totalStartPeriod + totalInMiddle + totalEndPeriod);
+            return totalStartPeriod + totalInMiddle + totalEndPeriod;
         }
 
         throw new RuntimeException("You should not be here.  We have returned all legitimate numbers from getAmount(Date, Date) in BudgetCategoryImpl.  Please contact Wyatt Olson with details on how you got here (what steps did you perform in Buddi to get this error message).");
